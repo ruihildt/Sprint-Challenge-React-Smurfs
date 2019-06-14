c8b350499c775d21fc355a7a3c07d03f2764ce93
ruihildt <ruihildt@protonmail.com>
2019-06-14 10:04:08 +0200

    Load smurfs from API

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+const smurfAPI = 'http://localhost:3333/smurfs/';
 
 class App extends Component {
   constructor(props) {
@@ -11,6 +14,26 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  fetchFriend = async () => {
+    try {
+      const axiosData = await axios.get(smurfAPI);
+      this.setState({ smurfs: axiosData.data });
+    } catch (err) {
+        this.setState({
+          errorMessage: err.message
+        });
+    } finally {
+      //Confirm data is loaded
+      this.state.smurfs && console.log('Smurf data successfully loaded');
+    }
+  }
+
+  componentDidMount() {
+    this.fetchFriend();
+  }
+
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
